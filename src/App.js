c0d1d87cdecae63abbd8d800193ba23013afc489
jefferson250598 <amarantegeraldelli@gmail.com>
2022-06-23 15:54:51 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import Img from "./img/inicio.png"
+import Logo from "./img/logo.png"
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    box-sizing: border-box;
+    padding: 0;
+  }`
+
+const Container = styled.div`
+  padding:2em;
+  `
+const Home = styled.div`
+    background-image: url(${Img});
+    background-position: center;
+    background-size:100%;
+    height: 133vh;
+    background-repeat: no-repeat;
+    
+
+  `
+const Menu = styled.div`
+  height: 10vh;
+  padding-left: 2vw;
+  list-style: none;
+  display:flex;
+  justify-content: space-between;
+  aling-items: center;
+
+   
+  `
+const BoxImg = styled.div`
+ display:flex;
+ align-items:center;
+ `
+const Lista = styled.div`
+  width: 30vw;
+  align-items: center;
+  display:flex;
+  justify-content: space-evenly;
+  font-size: Hurme Geometric Sans 1, Regular;
+  `
+
+
+
+const BoxTitle = styled.div`
+  height: 78vh;
+  display:flex;
+  justify-content:center; 
+  align-items: flex-end;  
+  font-size: 4vw; 
+  font-family: Hurme Geometric Sans 1, SemiBold;
+  color: #373737;
+ 
+ `
+
+
+
+
+export default function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyle />
+      <Container>
+        <Home>
+          <Menu>
+            <BoxImg>
+              <img src={Logo} width="45" />
+            </BoxImg>
+            <Lista>
+              <li>ABOUT</li>
+              <li>RECIPES</li>
+              <li>SUBSCRIBE</li>
+            </Lista>
+          </Menu>
+          <BoxTitle>
+            <h1>RECIPES</h1>
+          </BoxTitle>
+        </Home>
+      </Container>
+
     </div>
+
+
   );
 }
-
-export default App;
